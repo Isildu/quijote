dbc840c35a14d0d53382f019fbbe10cd2a62992b
Oriol Canellas <oriolcanellas3@gmail.com>
2026-04-28 12:57:17 +0200

    mejoras

--- js/index.js
+++ js/index.js
@@ -12,4 +12,8 @@ lector.leerArchivo('../el_quijote.txt', (error, texto) => {
   console.log(`Palabras totales: ${sabio.contarPalabras(texto)} (sin signos de puntuación)`);
   console.log('Top 10 palabras clave:');
   console.table(sabio.top10Palabras(texto)); 
+  console.log(`Avistamientos de Don Quijote: ${sabio.buscarPalabraEspecifica(texto, 'quijote')}`);
+  console.log(`Avistamientos de Sancho Panza: ${sabio.buscarPalabraEspecifica(texto, 'sancho')}`);
+  console.log(`Avistamientos de Dulcinea: ${sabio.buscarPalabraEspecifica(texto, 'dulcinea')}`);
+  console.log(`Avistamientos de Rocinante: ${sabio.buscarPalabraEspecifica(texto, 'rocinante')}`);
 });
